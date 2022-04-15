de62376f2488a7a8f1cc11f118e5b7085e3ec054
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-15 14:48:22 -0300

    Criando validações dentro do adonis

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -3,10 +3,33 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
+/*
+  {
+    "users": {
+      "id": number,
+      "email": string,
+      "username": string,
+      "password": string,
+      "avatar": string
+    }
+  }
+*/
+
 test.group('User', () => {
   test.only('it should create an user', async (assert) => {
-    const userPayload = { email: 'test@test.com', username: 'test', password: 'test' }
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'https://images.com/image/1',
+    }
     console.log(process.env.HOST)
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined') //Senão tiver um usuário, então retorna o "User undefined"
+    assert.exists(body.user.id, 'User undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.password, userPayload.password)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
