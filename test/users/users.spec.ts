843087008938df7fd03ba3cddbbf0c8b0c79847b
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-27 20:49:01 -0300

    criando usuario

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -21,15 +21,7 @@ test.group('User', () => {
       email: 'test@test.com',
       username: 'test',
       password: 'test',
-      avatar: 'https://images.com/image/1',
     }
-    console.log(process.env.HOST)
     const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
-    assert.exists(body.user, 'User undefined') //Senão tiver um usuário, então retorna o "User undefined"
-    assert.exists(body.user.id, 'User undefined')
-    assert.equal(body.user.email, userPayload.email)
-    assert.equal(body.user.username, userPayload.username)
-    assert.equal(body.user.password, userPayload.password)
-    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
