2eaed25a7da8eafe9d388d2b9e450d041bf67eb4
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-05-09 20:37:46 -0300

    Introdução ao Mailer: enviando emails com o Adonis

--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -2,6 +2,7 @@ import { UserFactory } from 'Database/factories';
 import Database from '@ioc:Adonis/Lucid/Database';
 import test, { group } from "japa";
 import supertest from 'supertest'; //Servidor de teste
+import Mail from '@ioc:Adonis/Addons/Mail';
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
@@ -10,10 +11,16 @@ test.group('Password', (group) => {
   test.only('it should send an email with forgot password instructions', async (assert) => {
     const user = await UserFactory.create()
 
+    Mail.trap((message) => {
+      assert.equal(message.subject, 'Roleplay: Recuperação de senha')
+    })
+
     await supertest(BASE_URL).post('/forgot-password').send({
       email: user.email,
       resetPasswordUrl: 'url'
     }).expect(204)
+
+    Mail.restore()
 
   })
 
