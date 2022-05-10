e7da9b8a1e8cc28245ef1e87fab88de10892fb2c
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-05-09 21:44:26 -0300

    resolvendo captura de email

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -1,9 +1,20 @@
+import Mail from '@ioc:Adonis/Addons/Mail';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PasswordsController {
 
   public async forgotPassword({ request, response}: HttpContextContract){
-    return response.noContent()
+
+    const { email } = request.only(['email'])
+
+    const sended = await Mail.send((message) => {
+      message
+        .from('no-reply@roleplay.com')
+        .to(email)
+        .subject('O envio de email deu certo')
+        .text('Clique no link abaixo para redefinir sua senha')
+    })
+
   }
 
 }
--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -3,6 +3,7 @@ import Database from '@ioc:Adonis/Lucid/Database';
 import test, { group } from "japa";
 import supertest from 'supertest'; //Servidor de teste
 import Mail from '@ioc:Adonis/Addons/Mail';
+import { Assert } from 'japa/build/src/Assert';
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
@@ -11,16 +12,10 @@ test.group('Password', (group) => {
   test.only('it should send an email with forgot password instructions', async (assert) => {
     const user = await UserFactory.create()
 
-    Mail.trap((message) => {
-      assert.equal(message.subject, 'Roleplay: Recuperação de senha')
-    })
-
     await supertest(BASE_URL).post('/forgot-password').send({
       email: user.email,
       resetPasswordUrl: 'url'
     }).expect(204)
-
-    Mail.restore()
 
   })
 
