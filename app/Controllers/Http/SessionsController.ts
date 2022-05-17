2f8baf14f69526583a5cce4a0c07944164489e7e
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-05-16 21:38:54 -0300

    Implementando o logout do usuário

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -3,13 +3,18 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class SessionsController {
 
-  public async store({request, response, auth}: HttpContextContract){
+  public async store({ request, response, auth }: HttpContextContract){
     const { email, password } = request.only(['email', 'password'])
     const token = await auth.use('api').attempt(email, password, {
       expiresIn: '2hours' //o token retornado expira em 2 horas
     }) //faz a autenticação do usuário e retorna o token
 
     return response.created({ user: auth.user, token })
+  }
+
+  public async destroy({ response, auth }: HttpContextContract){
+    await auth.logout()
+    return response.ok({}) //a funçãom .ok() significa 201
   }
 
 }
--- test/users/sessions.spec.ts
+++ test/users/sessions.spec.ts
@@ -40,7 +40,7 @@ test.group('Session', (group) => {
     assert.equal(body.status, 400)
   })
 
-  test.only('it should return 400 when credentials are invalid', async (assert) => {
+  test('it should return 400 when credentials are invalid', async (assert) => {
     const { email } = await UserFactory.create()
     const { body } = await supertest(BASE_URL).post('/sessions').send({
       email,
@@ -50,6 +50,54 @@ test.group('Session', (group) => {
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 400)
     assert.equal(body.message, 'invalid credentials')
+  })
+
+  test('it should return 200 when user signs out', async (assert) => {
+    const plainPassword = 'test'
+    const { email, id } = await UserFactory.merge({password: plainPassword}).create() //cria o usuário no bd
+
+    const { body } = await supertest(BASE_URL) //manda as credenciais para logar na api
+      .post('/sessions')
+      .send({email, password: plainPassword})
+      .expect(201)
+
+    const apiToken = body.token
+
+    await supertest(BASE_URL) //pedindo para sair da api, e otoken é apagado
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${apiToken.token}`) //Passando dados pelo cabeçalho da requisição
+      .expect(200)
+  })
+
+  test.only('it should revoke token when user signs out', async (assert) => {
+    const plainPassword = 'test'
+    const { email, id } = await UserFactory.merge({password: plainPassword}).create() //cria o usuário no bd
+
+    const { body } = await supertest(BASE_URL) //manda as credenciais para logar na api
+      .post('/sessions')
+      .send({email, password: plainPassword})
+      .expect(201)
+
+    const apiToken = body.token
+
+    const tokenBeforeSignout = await Database.query()
+      .select('*')
+      .from('api_tokens') //Pega o token antes dele ser deletado quando o usuário faz signout
+
+    console.log(tokenBeforeSignout)
+
+    await supertest(BASE_URL) //pedindo para sair da api, O LOGOUT É BASICAMENTE DELETAR A SESSÃO COM O TOKEN DO USUÁRIO
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${apiToken.token}`) //Passando dados pelo cabeçalho da requisição
+      .expect(200)
+
+    const token = await Database.query()
+      .select('*')
+      .from('api_tokens') //Seleciona tudo da tabela api_tokens onde token é igual ao token que o usuário recebeu quando fez login na api
+
+      console.log(token)
+    assert.isEmpty(token) //O token tem que e stá vazio, pois depois que o usuário faz logout o token dele é deletado
+
   })
 
   group.beforeEach(async () => {
